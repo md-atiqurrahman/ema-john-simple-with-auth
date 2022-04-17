456e5b0d9e7ff0f4dcd54c5d3b68d4b812e36836
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-04-18 03:09:07 +0600

    add enviroment variable

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -31,7 +31,7 @@ const Shipment = () => {
     return (
         <div className='form-container'>
         <div>
-            <h2 className='form-title'>Shipping Information</h2>
+            <h2 className='form-title'> Your Shipping Info</h2>
             <form onSubmit={handleClickOnAddShipment} className='form-fields'>
                 <div className="input-group">
                     <label htmlFor="name">Your Name</label>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBpOK69Sao2X-Ho5QcxSWOOuKSU4bA8Fg8",
-  authDomain: "ema-john-simple-with-aut-4e508.firebaseapp.com",
-  projectId: "ema-john-simple-with-aut-4e508",
-  storageBucket: "ema-john-simple-with-aut-4e508.appspot.com",
-  messagingSenderId: "430851420732",
-  appId: "1:430851420732:web:7c9df03076e3de0cdc0939"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
